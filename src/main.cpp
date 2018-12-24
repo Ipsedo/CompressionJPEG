f0607daeb060461803500edf72c340e801c42380
Ipsedo <berriens@gmail.com>
2018-12-24 14:24:28 +0100

    continué main

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
+#include "bmp/read_bmp.h"
 
 int main(int argc, char **argv) {
-  std::cout << "Compression JPEG" << std::endl;
+  if (argc < 2) {
+    std::cout << "arg 1 must be the bmp file name to convert" << std::endl;
+  }
+
+  std::string fileName = argv[1];
+  imgRGB img = readBMP(fileName);
+
+  std::cout << "Img width : " << img.width << std::endl;
+  std::cout << "Img height : " << img.height << std::endl;
+
   return 0;
 }
