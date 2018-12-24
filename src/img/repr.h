32767204ce174341366a377b96c612b6deac510e
Ipsedo <berriens@gmail.com>
2018-12-24 15:52:21 +0100

    add color type

--- src/img/repr.h
+++ src/img/repr.h
@@ -6,10 +6,14 @@
 #define COMPRESSIONJPEG_REPR_H
 
 #include <vector>
+#include <tuple>
+
+typedef std::tuple<int, int, int> color;
+
 struct imgRGB {
     int width;
     int height;
-    std::vector<std::vector<char>> pixels;
+    std::vector<std::vector<color>> pixels;
 };
 
 #endif //COMPRESSIONJPEG_REPR_H
