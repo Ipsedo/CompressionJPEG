835f62d511f98558530a3232bd414c18e4c9b5e6
Ipsedo <berriens@gmail.com>
2018-12-26 18:08:04 +0100

    test DCT -> OK mais pb precision numerique

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include "bmp/read_bmp.h"
 #include "img/img_misc.h"
-
+#include "dct/idct.h"
+#include "dct/fdct.h"
+#include <vector>
 
 int main(int argc, char **argv) {
-  if (argc < 2) {
+  /*if (argc < 2) {
     std::cout << "arg 1 must be the bmp file name to convert" << std::endl;
   }
 
@@ -14,7 +16,37 @@ int main(int argc, char **argv) {
   std::cout << "Img width : " << img.width << std::endl;
   std::cout << "Img height : " << img.height << std::endl;
 
-  printImg(img, img.width, img.height);
+  printImg(img, img.width, img.height);*/
+
+  std::vector<std::vector<double>> r;
+  for (int i = 0; i < 10; i++) {
+      std::vector<double> tmp;
+      for (int j = 0; j < 10; j++) {
+          tmp.push_back(i * j);
+      }
+      r.push_back(tmp);
+  }
+
+  std::vector<std::vector<double>> dct;
+  for (int i = 0; i < 10; i++) {
+      std::vector<double> tmp;
+      for (int j = 0; j < 10; j++) {
+          std::cout << r[i][j] << ", ";
+          tmp.push_back(fdct(r, i, j, 10));
+      }
+      std::cout << std::endl;
+      dct.push_back(tmp);
+  }
+  std::cout << std::endl;
+
+  std::vector<std::vector<double>> invdct;
+  for (int i = 0; i < 10; i++) {
+      for (int j = 0; j < 10; j++) {
+          std::cout << idct(dct, i, j, 10) << ", ";
+      }
+      std::cout << std::endl;
+  }
+
 
   return 0;
 }
