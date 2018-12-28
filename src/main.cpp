033eedf780bed3546fee902ef172b6498d2525a2
Ipsedo <berriens@gmail.com>
2018-12-28 15:18:08 +0100

    test matrix from wikipedia -> OK

--- src/main.cpp
+++ src/main.cpp
@@ -23,14 +23,15 @@ int main(int argc, char **argv) {
 
 	int N = 8;
 
-	std::vector<std::vector<double>> r;
-	for (int i = 0; i < N; i++) {
-		std::vector<double> tmp;
-		for (int j = 0; j < N; j++) {
-			tmp.push_back(i * j);
-		}
-		r.push_back(tmp);
-	}
+	// from wikipedia https://fr.wikipedia.org/wiki/JPEG
+	std::vector<std::vector<double>> r = { {139,144,149,153,155,155,155,155},
+									 {144,151,153,156,159,156,156,156},
+									 {150,155,160,163,158,156,156,156},
+									 {159,161,162,160,160,159,159,159},
+									 {159,160,161,162,162,155,155,155},
+									 {161,161,161,161,160,157,157,157},
+									 {162,162,161,163,162,157,157,157},
+									 {162,162,161,161,163,158,158,158} };
 
 	std::cout << "Original : " << std::endl;
 	std::vector<std::vector<double>> dct;
