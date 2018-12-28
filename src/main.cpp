1a236111bb8dfc7f5841ab8d584328420acc9e38
Ipsedo <berriens@gmail.com>
2018-12-29 00:35:53 +0100

    test zig zag

--- src/main.cpp
+++ src/main.cpp
@@ -6,32 +6,31 @@
 #include "quant/quantization.h"
 #include "quant/quant_matrix.h"
 #include "quant/de_quantization.h"
+#include "enc/zig_zag.h"
 #include <vector>
+#include <iomanip>
 
-int main(int argc, char **argv) {
-	/*if (argc < 2) {
-	  std::cout << "arg 1 must be the bmp file name to convert" << std::endl;
-	}
-
-	std::string fileName = argv[1];
-	imgRGB img = readBMP(fileName);
+void test_bmp(std::string file_name) {
+	imgRGB img = readBMP(file_name);
 
 	std::cout << "Img width : " << img.width << std::endl;
 	std::cout << "Img height : " << img.height << std::endl;
 
-	printImg(img, img.width, img.height);*/
+	printImg(img, img.width, img.height);
+}
 
+void test_all() {
 	int N = 8;
 
 	// from wikipedia https://fr.wikipedia.org/wiki/JPEG
 	std::vector<std::vector<double>> r = { {139,144,149,153,155,155,155,155},
-									 {144,151,153,156,159,156,156,156},
-									 {150,155,160,163,158,156,156,156},
-									 {159,161,162,160,160,159,159,159},
-									 {159,160,161,162,162,155,155,155},
-									 {161,161,161,161,160,157,157,157},
-									 {162,162,161,163,162,157,157,157},
-									 {162,162,161,161,163,158,158,158} };
+										   {144,151,153,156,159,156,156,156},
+										   {150,155,160,163,158,156,156,156},
+										   {159,161,162,160,160,159,159,159},
+										   {159,160,161,162,162,155,155,155},
+										   {161,161,161,161,160,157,157,157},
+										   {162,162,161,163,162,157,157,157},
+										   {162,162,161,161,163,158,158,158} };
 
 	std::cout << "Original : " << std::endl;
 	std::vector<std::vector<double>> dct;
@@ -84,6 +83,49 @@ int main(int argc, char **argv) {
 		}
 		std::cout << std::endl;
 	}
+
+}
+
+void test_zig_zag() {
+	int N = 8;
+
+	int curr = 0;
+	std::vector<std::vector<int>> v;
+
+	std::cout << "Original :" << std::endl;
+	for (int i = 0; i < N; i++) {
+		std::vector<int> line;
+		for (int j = 0; j < N; j++) {
+			line.push_back(curr);
+			std::cout << std::setfill(' ') << std::setw(2) << curr << ", ";
+			curr++;
+		}
+		std::cout << std::endl;
+		v.push_back(line);
+	}
+	std::cout << std::endl;
+
+	std::vector<int> res = zig_zag_encodage(v);
+
+	for (int i = 0; i < res.size(); i++)
+		std::cout << res[i] << ", ";
+	std::cout << std::endl;
+}
+
+int main(int argc, char **argv) {
+
+	/*if (argc < 2) {
+	  std::cout << "arg 1 must be the bmp file name to convert" << std::endl;
+	}
+
+	test_bmp(argv[1]);
+	*/
+
+	/*
+	test_all();
+	 */
+
+	test_zig_zag();
 
 	return 0;
 }
