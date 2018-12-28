7a6bd6ac48c43a76a2afa522ec95830905a34168
Ipsedo <berriens@gmail.com>
2018-12-28 14:51:08 +0100

    add quantization matrix Q1

---
+++ src/quant/quant_matrix.h
@@ -0,0 +1,20 @@
+//
+// Created by samuel on 28/12/18.
+//
+
+#ifndef COMPRESSIONJPEG_QUANT_MATRIX_H
+#define COMPRESSIONJPEG_QUANT_MATRIX_H
+
+#include <vector>
+
+// From wikipedia https://fr.wikipedia.org/wiki/JPEG#Quantification
+#define Q1 std::vector<std::vector<double>> { {16,11,10,16,24,40,51,61}, \
+                                              {12,12,14,19,26,58,60,55}, \
+                                              {14,13,16,24,40,57,69,56}, \
+                                              {14,17,22,29,51,87,80,62}, \
+                                              {18,22,37,56,68,109,103,77}, \
+                                              {24,35,55,64,81,104,113,92}, \
+                                              {49,64,78,87,103,121,120,101}, \
+                                              {72,92,95,98,112,100,103,99} }
+
+#endif //COMPRESSIONJPEG_QUANT_MATRIX_H
