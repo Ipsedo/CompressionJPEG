89d04faea097aa1c7f52be4b30fc4f0f850c3c75
Ipsedo <berriens@gmail.com>
2018-12-28 14:50:06 +0100

    fix limit for

--- src/quant/quantization.cpp
+++ src/quant/quantization.cpp
@@ -10,7 +10,7 @@ std::vector<std::vector<double>> quantize(std::vector<std::vector<double>> F, st
 
     for (int u = 0; u < F.size(); u++) {
         std::vector<double> line;
-        for (int v = 0; v < F.size(); v++) {
+        for (int v = 0; v < F[u].size(); v++) {
             line.push_back(round(F[u][v] / Q[u][v]));
         }
         res.push_back(line);
