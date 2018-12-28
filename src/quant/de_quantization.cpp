e6b28374f9784b89a28178a29f4a4bbce7cfbbc2
Ipsedo <berriens@gmail.com>
2018-12-28 14:51:30 +0100

    add de-quantization function

---
+++ src/quant/de_quantization.cpp
@@ -0,0 +1,20 @@
+//
+// Created by samuel on 28/12/18.
+//
+
+#include "de_quantization.h"
+
+
+std::vector<std::vector<double>> de_quantize(std::vector <std::vector<double>> F_prime, std::vector <std::vector<double>> Q) {
+    std::vector<std::vector<double>> res;
+
+    for (int u = 0; u < F_prime.size(); u++) {
+        std::vector<double> line;
+        for (int v = 0; v < F_prime[u].size(); v++) {
+            line.push_back(F_prime[u][v] * Q[u][v]);
+        }
+        res.push_back(line);
+    }
+
+    return res;
+}
---
+++ src/quant/de_quantization.h
@@ -0,0 +1,12 @@
+//
+// Created by samuel on 28/12/18.
+//
+
+#ifndef COMPRESSIONJPEG_DE_QUANTIZATION_H
+#define COMPRESSIONJPEG_DE_QUANTIZATION_H
+
+#include <vector>
+
+std::vector<std::vector<double>> de_quantize(std::vector<std::vector<double>> F_prime, std::vector<std::vector<double>> Q);
+
+#endif //COMPRESSIONJPEG_DE_QUANTIZATION_H
