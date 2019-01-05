1a83dcedc0c2eefda682b050c530e44bd636c19a
Ipsedo <berriens@gmail.com>
2019-01-05 15:11:37 +0100

    rle -> marche presque (pb 0 au milieu)

---
+++ src/enc/rle.cpp
@@ -0,0 +1,53 @@
+//
+// Created by samuel on 05/01/19.
+//
+
+#include "rle.h"
+#include "../jpeg/markers.h"
+#include <math.h>
+#include <iostream>
+
+std::vector<char> rle(std::vector<int> quantized_block) {
+	std::vector<char> res;
+
+	for (int i = 0; i < quantized_block.size(); i++) {
+		bool all_zero = true;
+		int nb_zero = 0;
+
+		for (int j = i; j < quantized_block.size(); j++) {
+			if (abs(quantized_block[j]) != 0) {
+				all_zero = false;
+				break;
+			} else
+				nb_zero++;
+		}
+		if (all_zero) {
+			i = quantized_block.size();
+			res.push_back(EOB);
+		} else {
+			int zero_restant = nb_zero;
+			if (zero_restant >= 16) {
+				res.push_back(ZRL);
+				zero_restant -= 16;
+				i += 16;
+			}
+			if (zero_restant > 16) {
+				std::cout << "pb all zero > 16" << std::endl;
+				exit(1);
+			}
+			if (zero_restant > 0) {
+				// TODO 5 zeros suivi d'un 2 -> 0x52
+				// TODO Marche pas
+				auto z = (unsigned char) zero_restant;
+				unsigned char to_write = 0 | z >> 4;
+				i += zero_restant;
+				to_write |= (unsigned char) quantized_block[i];
+				res.push_back(to_write);
+			} else {
+				res.push_back((char) quantized_block[i]);
+			}
+		}
+	}
+
+	return res;
+}
---
+++ src/enc/rle.h
@@ -0,0 +1,12 @@
+//
+// Created by samuel on 05/01/19.
+//
+
+#ifndef COMPRESSIONJPEG_RLE_H
+#define COMPRESSIONJPEG_RLE_H
+
+#include <vector>
+
+std::vector<char> rle(std::vector<int> quantized_block);
+
+#endif //COMPRESSIONJPEG_RLE_H
