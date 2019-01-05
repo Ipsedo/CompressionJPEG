5835532dbc337a3400d3d8b82970822b931b8a45
Ipsedo <berriens@gmail.com>
2019-01-05 18:05:19 +0100

    copied write bmp function from stackoverflow

---
+++ src/bmp/write_bmp.h
@@ -0,0 +1,13 @@
+//
+// Created by samuel on 05/01/19.
+//
+
+#ifndef COMPRESSIONJPEG_WRITE_BMP_H
+#define COMPRESSIONJPEG_WRITE_BMP_H
+
+#include "../img/repr.h"
+#include <string>
+
+void write_grey_scale(imgGreyScale img, std::string output_file);
+
+#endif //COMPRESSIONJPEG_WRITE_BMP_H
